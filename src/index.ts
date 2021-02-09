a5d9c54a2843069456a4be3f644486b08aec9395
Rusbert8 <rusbertsanchez8@gmail.com>
2021-02-09 09:18:59 -1200

    Insertando usuarios en firestore

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-import "./firebase/config";
+import db from "./firebase/config";
+
+const usuario = {
+    nombre: "Rusbert",
+    activos: true,
+    nacimiento: 0
+}
+
+db.collection("Usuarios")
+    .add(usuario)
+    .then(docReft=>{
+        console.log(docReft.delete)
+    })
+    .catch(e=>console.log("error", e))
