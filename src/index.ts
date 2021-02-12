71dab0db3dc564c62f9dba9cfa63530e692a9df9
Rusbert8 <rusbertsanchez8@gmail.com>
2021-02-11 16:54:38 -1200

    Ordenar registros seleccionados

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,45 @@ import db from "./firebase/config";
 import firebase from "firebase";
 
 // CREAR tabla:
+// Create table usuario { 
+//     nombre: varchar ("Rusbert"),
+//     activos: boolean (true),
+//     nacimiento: int (0)
+// }
 
     // const usuario = {
     //     nombre: "Rusbert",
     //     activos: true,
-    //     nacimiento: 0
+    //     nacimiento: 20
     // }
 
 // Referencia a la tabla: 
 
     const usuarios_ref = db.collection("Usuarios");
 
-
-//INSERTAR campo:
+//INSERTAR campo o registro:
+// (Insert into usuarios)
 
     // usuarios_ref
     //     .add(usuario)
     
 // ACTUALIZAR o MODIFICAR campos:
+// (Update usuarios set activos = false where id = identificador)
 
     // usuarios_ref.doc("ID del campo a modificar")
     //     .update({
     //         pares de atributos-valor a modificar
+    //         (activos = false)
     //     })
 
-// BORRAR campos: (Delete )
+// BORRAR campos: 
+// (Delete from usuarios where id = identificador)
 
     // usuarios_ref.doc("ID del campo a borrar")
     //      .delete();
 
-// SELECCIONAR todos los registros de una tabla: (SELECT * from "tabla")
+// SELECCIONAR todos los registros de una tabla: 
+// (SELECT * from "tabla")
 
     const mostrar_registros = (snap: firebase.firestore.QuerySnapshot) => {
             const documentos: any[] = [];
@@ -54,18 +63,29 @@ import firebase from "firebase";
 // haciendo referencia a un solo campo:
 // (SELECT * from usuarios WHERE nacimiento > 0 AND nacimiento < 10)
 
-    usuarios_ref
-            .where("nacimiento",">", 0)
-            .where("nacimiento","<", 10)
-        .onSnapshot(mostrar_registros);
+    // usuarios_ref
+    //         .where("nacimiento",">", 0)
+    //         .where("nacimiento","<", 10)
+    //     .onSnapshot(mostrar_registros);
 
 // SELECCIONAR registros de una tabla por condiciones,
 // haciendo referencia a varios campos:
 // (SELECT * from usuarios WHERE activos == false AND nacimiento > 0)
 
-       usuarios_ref
-            .where("activos","==", false)
-            .where("nacimiento",">", 0)
-        .onSnapshot(mostrar_registros);
+    //    usuarios_ref
+    //         .where("activos","==", false)
+    //         .where("nacimiento",">", 0)
+    //     .onSnapshot(mostrar_registros);
 
 // Nota: primero hay que crear los índices.
+
+// ORDENAR registros seleccionados:
+// (SELECT * from usuarios ORDER BY nombre)
+
+    usuarios_ref
+        .orderBy("nombre")
+        .orderBy("nacimiento", "desc")
+        .onSnapshot(mostrar_registros);
+
+// Nota: si un registro tiene vacío el campo como referente, este no se mostrará.
+// Para ordenar de forma descendente colocamos "desc" al final del .orderBy().
